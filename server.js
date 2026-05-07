3487ef2c9e6d35f14852b19d13fb8b5d3049ea4d
gonzaagoni <gonzaagoni@gmail.com>
2026-05-07 03:29:02 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+﻿const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/mensaje', (req, res) => {
+
+    console.log('Mensaje recibido:');
+    console.log(req.body);
+
+    res.send('ok');
+
+});
+
+app.get('/', (req, res) => {
+
+    res.sendFile(__dirname + '/index.html');
+
+});
+
+app.listen(3000, () => {
+
+    console.log('Servidor iniciado');
+
+});  
