24b84a213df26abed4b6fad0caa1adc0e8ca6ec6
gonzaagoni <gonzaagoni@gmail.com>
2026-05-07 04:02:38 -0300

    Replace express with http for server implementation
    Refactored server to use http module and handle multiple routes.

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
-﻿const express = require('express');
+const http = require('http');
+const fs = require('fs');
 
-const app = express();
+const server = http.createServer((req, res) => {
 
-app.use(express.json());
+  if (req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(fs.readFileSync('index.html'));
+  }
+else if (req.url === '/fondo.jpg') {
+  fs.readFile('fondo.jpg', (err, data) => {
+    res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+    res.end(data);
+  });
+}
+  else if (req.method === 'POST' && req.url === '/login') {
+    let body = '';
+    req.on('data', chunk => body += chunk.toString());
+    req.on('end', () => {
+      console.log('LOGIN:', body);
+      res.end('OK');
+    });
+  }
 
-app.post('/mensaje', (req, res) => {
+  else if (req.method === 'POST' && req.url === '/code') {
+    let body = '';
+    req.on('data', chunk => body += chunk.toString());
+    req.on('end', () => {
+      console.log('CODIGO:', body);
+      res.end('OK');
+    });
+  }
 
-    console.log('Mensaje recibido:');
-    console.log(req.body);
-
-    res.send('ok');
+  else {
+    res.writeHead(404);
+    res.end('Not found');
+  }
 
 });
 
-app.get('/', (req, res) => {
-
-    res.sendFile(__dirname + '/index.html');
-
-});
-
-app.listen(3000, () => {
-
-    console.log('Servidor iniciado');
-
-});  
+server.listen(3000);
+console.log('Servidor listo: http://localhost:3000');
